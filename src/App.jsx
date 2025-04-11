7bba97ca6d8eafbfb826fdcdd559bc59235a2762
syedtayab321 <tayabsajid5@gmail.com>
2025-04-11 14:36:03 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
-
+import { Routes,Route } from 'react-router-dom'
 function App() {
 
   return (
     <>
-      
+      <Routes>
+           {/* all routes write here */}
+      </Routes>
     </>
   )
 }
